f3ba49360f6913b49531642db7f9dbcf1fcd69fe
layon940 <133156052+layon940@users.noreply.github.com>
2025-08-21 23:01:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN = "TU_TOKEN_DE_TELEGRAM"
-CREATOR_ID = 123456789  # Tu ID de Telegram
-VIDEO_API_URL = "http://up.hydrax.net/XXXXXXXXXXX"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CREATOR_ID = int(os.getenv("CREATOR_ID", "123456789"))
+VIDEO_API_URL = os.getenv("HYDRAX_API_URL")

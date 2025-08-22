594983360d10d5de411700f550f47ae1f82a423f
layon940 <133156052+layon940@users.noreply.github.com>
2025-08-22 00:45:15 -0400

    Update progress.py

--- utils/progress.py
+++ utils/progress.py
@@ -9,5 +9,16 @@ def generate_progress_bar(percent):
 def get_system_stats():
     cpu = psutil.cpu_percent()
     ram = psutil.virtual_memory().percent
-    free = round(shutil.disk_usage("/").free / (1024**3), 2)
-    return cpu, ram, free
+    free_bytes = shutil.disk_usage("/").free
+    return cpu, ram, free_bytes
+
+def format_size(size, show_bytes=True):
+    # size en bytes
+    if size < 1024:
+        return f"{size} B" if show_bytes else "0.0 MB"
+    elif size < 1024 ** 2:
+        return f"{size / 1024:.2f} KB"
+    elif size < 1024 ** 3:
+        return f"{size / (1024 ** 2):.2f} MB"
+    else:
+        return f"{size / (1024 ** 3):.2f} GB"

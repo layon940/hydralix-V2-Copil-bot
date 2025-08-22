c184693f18ccbc6a08ac8ee322008e77bbea2798
layon940 <133156052+layon940@users.noreply.github.com>
2025-08-22 04:30:19 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CREATOR_ID = int(os.getenv("CREATOR_ID", "123456789"))
-VIDEO_API_URL = os.getenv("HYDRAX_API_URL")
+CREATOR_ID = int(os.getenv("CREATOR_ID"))
+API_ID = int(os.getenv("API_ID"))     # my.telegram.org api_id
+API_HASH = os.getenv("API_HASH")      # my.telegram.org api_hash
+PYRO_SESSION = "large_session"        # nombre del archivo de sesión pyrogram

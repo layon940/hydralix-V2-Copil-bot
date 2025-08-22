d338be63237ccca30359d7767e17f3def23a753b
layon940 <133156052+layon940@users.noreply.github.com>
2025-08-21 21:47:22 -0400

    Update progress.py

--- utils/progress.py
+++ utils/progress.py
@@ -1,1 +1,13 @@
+import psutil
+import shutil
 
+def generate_progress_bar(percent):
+    filled = int(percent / 5)
+    empty = 20 - filled
+    return '⬢' * filled + ' ' * empty
+
+def get_system_stats():
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
+    free = round(shutil.disk_usage("/").free / (1024**3), 2)
+    return cpu, ram, free
